895a610010d12d71376b260bf6e5dcd8f78242fb
Андрій Малишкін <andrii.malyshkin@nure.ua>
2023-11-03 19:09:27 +0200

    Delete

--- Lineal_algebra/formatDouble.cpp
+++
@@ -1,5 +0,0 @@
-#include "formatDouble.h"
-
-double format(double number) {
-    return std::round(number * 100) / 100.0;
-}
--- Lineal_algebra/formatDouble.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-#include "Header.h"
-
-double format(double number);
